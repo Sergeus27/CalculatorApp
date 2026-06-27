291610578aa79f8d6057a489f2850913385f1788
Sergeus27 <7vgy@mail.ru>
2026-06-27 14:42:21 +0300

    перенос прогресса

--- CalculatorAppBackend/internal/handlers/calculationHandlers.go
+++ CalculatorAppBackend/internal/handlers/calculationHandlers.go
@@ -1,1 +1,13 @@
 package handlers
+
+import "calculator-app/internal/calculationService"
+
+type CalculationHandler struct {
+	service calculationService.CalculationService
+}
+
+func NewCalculationHandler(s calculationService.CalculationService) *CalculationHandler {
+	return &CalculationHandler{
+		service: s,
+	}
+}
